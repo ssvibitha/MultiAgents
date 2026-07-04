f62fedbe9e88662ab204624d09bc0404faeaa473
ssvibitha <vibithavarshini7@gmail.com>
2026-07-04 22:52:06 +0530

    modified scripts/seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -43,10 +43,6 @@ async function main() {
       categoryId: indoor.id,
       images: [
         {
-          url : "https://m.media-amazon.com/images/I/61tFPeyRujL._AC_UF1000,1000_QL80_.jpg",
-          alt: "Monstera Deliciosa",
-        },  
-        {
           url: "https://images.unsplash.com/photo-1657628397617-5b2352d6a881?auto=format&fit=crop&q=80&w=800",
           alt: "Monstera Deliciosa in living room",
         }
@@ -67,13 +63,9 @@ async function main() {
       categoryId: indoor.id,
       images: [
         {
-          url: "https://theaffordableorganicstore.com/cdn/shop/files/3cde1db0-e2f1-4f80-a38f-4afa1dd013a1_20260302_234057_0000.webp?v=1772700970",
+          url: "https://images.unsplash.com/photo-1687552212914-03a30c82053c?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           alt: "Snake Plant",
-        },
-        {
-          url: "https://images.squarespace-cdn.com/content/v1/54fbb611e4b0d7c1e151d22a/1610074066643-OP8HDJUWUH8T5MHN879K/Snake+Plant.jpg?format=1000w",
-          alt: "Snake Plant in modern interior",
-        },
+        }
       ],
       tags: [
         "air-purifying",
@@ -116,11 +108,11 @@ async function main() {
       categoryId: indoor.id,
       images: [
         {
-          url: "https://radhakrishnaagriculture.in/cdn/shop/files/peacelily.jpg?v=1709184309",
+          url: "https://images.unsplash.com/photo-1463320726281-696a485928c7?auto=format&fit=crop&w=800&q=80",
           alt: "Peace Lily",
         },
         {
-          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Spathiphyllum_cochlearispathum_RTBG.jpg/960px-Spathiphyllum_cochlearispathum_RTBG.jpg",
+          url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
           alt: "Peace Lily in modern interior",
         }
       ],
@@ -140,11 +132,11 @@ async function main() {
       categoryId: indoor.id,
       images: [
         {
-          url: "https://m.media-amazon.com/images/I/61eUcX92H6L._AC_UF1000,1000_QL80_.jpg",
+          url: "https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&fit=crop&w=800&q=80",
           alt: "Jade Plant",
         },
         {
-          url: "https://m.media-amazon.com/images/I/71psCqI6EDL.jpg",
+          url: "https://images.unsplash.com/photo-1512428813834-c702c7702b78?auto=format&fit=crop&w=800&q=80",
           alt: "Jade Plant care"
         }
       ],
@@ -165,11 +157,11 @@ async function main() {
       categoryId: outdoor.id,
       images: [
         {
-          url: "https://www.urvann.com/s/6176774ef575bbd2b3331c8a/693d42773c203ee75005dcf5/261-640x640.jpg",
+          url: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?auto=format&fit=crop&w=800&q=80",
           alt: "Rose Plant",
         },
         {
-          url: "https://nurserylive.com/cdn/shop/files/rose-scented_b8d09b33-17d1-40b6-bb69-dcf2b3860e81.jpg?v=1751753898",
+          url: "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?auto=format&fit=crop&w=800&q=80",
           alt: "Rose Plant in garden",
         }
       ],
@@ -211,11 +203,11 @@ async function main() {
       categoryId: outdoor.id,
       images: [
           {
-            url: "https://creativefarmer.in/cdn/shop/products/61ubDZoQc4L._SL1024.jpg?v=1647501869",
+            url: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?auto=format&fit=crop&w=800&q=80",
             alt: "Jasmine"
           },
           {
-            url: "https://himadrigardens.com/wp-content/uploads/2024/07/WhatsApp-Image-2024-07-07-at-6.39.32-PM.jpeg",
+            url: "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
             alt: "Climbing jasmine plant"
           }
       ],
@@ -235,11 +227,11 @@ async function main() {
       categoryId: outdoor.id,
       images: [
           {
-            url: "https://plantsguru.com/cdn/shop/files/Mango_Hapus_Plant.jpg?v=1746859106&width=3840",
+            url: "https://images.unsplash.com/photo-1464226184884-fa280b87c399?auto=format&fit=crop&w=800&q=80",
             alt: "Mango Plant"
           },
           {
-            url: "https://5.imimg.com/data5/VV/KU/MY-11242929/natural-gir-kesar-mangoes-500x500-500x500.jpg",
+            url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
             alt: "Mango tree with fruits"
           }
       ],
@@ -258,11 +250,11 @@ async function main() {
       categoryId: outdoor.id,
       images: [
           {
-            url: "https://www.newnessplant.com/uploads/bfdb03009feaa96390950dfd02b52b45.JPG",
+            url: "https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=800&q=80",
             alt: "Lemon Plant"
           },
           {
-            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbhljfBY0za8Kyw6DzchG9JNQGxiE3n7fC1A&s",
+            url: "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?auto=format&fit=crop&w=800&q=80",
             alt: "Lemon Plant in garden"
           }
       ],
@@ -281,11 +273,11 @@ async function main() {
       categoryId: indoor.id,
       images: [
           {
-            url: "https://nurserynisarga.in/wp-content/uploads/2019/03/areca.webp",
+            url: "https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&fit=crop&w=800&q=80",
             alt: "Areca Palm"
           },
           {
-            url: "https://theaffordableorganicstore.com/cdn/shop/files/dacf3142-dd59-48c5-9cc8-a827abd85d20.webp?v=1777556862",
+            url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
             alt: "Areca palm guide"
           }
       ],
@@ -306,11 +298,11 @@ async function main() {
       categoryId: seeds.id,
       images: [
         {
-          url: "https://organicbazar.net/cdn/shop/files/OkraorLadyFingerSeeds_okraseeds_ladyfingerseeds_vegetableseeds_organicbazarseeds_bhndiKeBeej.jpg?v=1770811840&width=1445",
+          url: "https://images.unsplash.com/photo-1464226184884-fa280b87c399?auto=format&fit=crop&w=800&q=80",
           alt: "Ladyfinger seeds packet"
         },
         {
-          url: "https://shop.indiagardening.com/wp-content/uploads/2020/08/lady-finger.jpg",
+          url: "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?auto=format&fit=crop&w=800&q=80",
           alt: "Ladyfinger seeds to plants"
         }
       ],
@@ -329,11 +321,11 @@ async function main() {
       categoryId: seeds.id,
       images: [
         {
-          url: "https://organicbazar.net/cdn/shop/files/Chilli_Light_Green_Medium_Spicy_Seeds_organic_bazar_seeds_chilli_seeds_green_chilli_seeds_chilli_green_light_seeds_vegetable_seeds.jpg?v=1759558371",
+          url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
           alt: "Chilli seeds packet"
         },
         {
-          url: "https://img.drz.lazcdn.com/static/lk/p/a4256bc619f3455b07f7ca389d59f228.jpg_720x720q80.jpg",
+          url: "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
           alt: "Chilli seeds to plants"
         }
       ],
@@ -352,11 +344,11 @@ async function main() {
       categoryId: seeds.id,
       images: [
         {
-          url: "https://organicbazar.net/cdn/shop/files/SunflowerTallYellowSeedsOrganicbazarSeedsOrganicbazarFlowerSeeds_1.jpg?v=1755770926&width=1946",
+          url: "https://images.unsplash.com/photo-1463320726281-696a485928c7?auto=format&fit=crop&w=800&q=80",
           alt: "Sunflower seeds packet"
         },
         {
-          url: "https://nativeindianorganics.com/cdn/shop/files/How_to_grow_sunflower_seeds.webp?v=1772004556&width=1000",
+          url: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?auto=format&fit=crop&w=800&q=80",
           alt: "How to grow sunflower seeds"
         }
       ],
@@ -376,11 +368,11 @@ async function main() {
       categoryId: seeds.id,
       images: [
         {
-          url: "https://www.levels.com/_next/image?url=https%3A%2F%2Fstatic.levels.com%2F_next%2Fstatic%2Fmedia%2FBasilSeeds.1da20cf5.jpg&w=3840&q=75",
+          url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
           alt: "Basil seeds"
         },
         {
-          url: "https://organicbazar.net/cdn/shop/files/IndianHolyBasil_DesiTulsi_Seeds_DesiTulsiKeBeej_TulsiKeBeej_BasilSeeds.jpg?v=1758176591",
+          url: "https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&fit=crop&w=800&q=80",
           alt: "Basil seeds packet"
         }
       ],
@@ -400,11 +392,11 @@ async function main() {
       categoryId: seeds.id,
       images: [
         {
-          url: "https://m.media-amazon.com/images/I/71qxsULIGBL._AC_UF1000,1000_QL80_.jpg",
+          url: "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?auto=format&fit=crop&w=800&q=80",
           alt: "Microgreens mix"
         },
         {
-          url: "https://rukmini1.flixcart.com/image/1500/1500/xif0q/plant-seed/b/m/c/1-microgreens-kit-nurturing-green-original-imahhm2zxp6gf3rd.jpeg?q=70",
+          url: "https://images.unsplash.com/photo-1459156212016-c812468e2115?auto=format&fit=crop&w=800&q=80",
           alt: "Microgreens growing kit"
         }
       ],
@@ -425,11 +417,11 @@ async function main() {
       categoryId: tools.id,
       images: [
         {
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF8aNLb4tmKti_P_HhLGuJHiiiJnDrBcjG7Q&s",
+          url: "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?auto=format&fit=crop&w=800&q=80",
           alt: "Garden trowel",
         },
         {
-          url: "https://makerbazar.in/cdn/shop/files/HandDiggingTrowel_Steel-6.jpg?v=1770545428&width=1100",
+          url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
           alt: "Garden trowel in use",
         }
       ],
@@ -448,11 +440,11 @@ async function main() {
       categoryId: tools.id,
       images: [
         {
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQuPyg-C2RCWVJUaRNg7N4RtRMv-ybFNOEug&s",
+          url: "https://images.unsplash.com/photo-1464226184884-fa280b87c399?auto=format&fit=crop&w=800&q=80",
           alt: "Pruning shears with ergonomic handle",
         },
         {
-          url: "https://plastrip.co.za/cdn/shop/files/WEBDSC_6444editDura5PruningShearjpeg_600x.jpg?v=1701413406",
+          url: "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
           alt: "Pruning shears in use"
         }
       ],
@@ -471,11 +463,11 @@ async function main() {
       categoryId: tools.id,
       images: [
         {
-          url: "https://m.media-amazon.com/images/I/51c5crjFdkL._AC_UF894,1000_QL80_.jpg",
+          url: "https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&fit=crop&w=800&q=80",
           alt: "Watering can for plants"
         },
         {
-          url: "https://dukaan.b-cdn.net/700x700/webp/download-and-upload/d288c911-2c24-494c-9667-23a8e6a95877.jpeg",
+          url: "https://images.unsplash.com/photo-1512428813834-c702c7702b78?auto=format&fit=crop&w=800&q=80",
           alt: "Watering plants with the watering can"
         }
       ],
@@ -494,11 +486,11 @@ async function main() {
       categoryId: tools.id,
       images: [
         {
-          url: "https://www.carbongold.com/app/uploads/2021/05/JI_100713_CarbonGold_012.jpg",
+          url: "https://images.unsplash.com/photo-1463320726281-696a485928c7?auto=format&fit=crop&w=800&q=80",
           alt: "Organic compost",
         },
         {
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK2CRYwUTP9ag5JgQsFmrRtkQhEAZjwPiNCg&s",
+          url: "https://images.unsplash.com/photo-1459156212016-c812468e2115?auto=format&fit=crop&w=800&q=80",
           alt: "Organic compost bag"
         }
       ],
@@ -517,11 +509,11 @@ async function main() {
       categoryId: tools.id,
       images:[
         {
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfynx_I1cuaNqnqDuEYKilRTGvaQXY30bjZA&s",
+          url: "https://images.unsplash.com/photo-1512428813834-c702c7702b78?auto=format&fit=crop&w=800&q=80",
           alt: "Grow light with full spectrum",
         },
         {
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJAkww9RdZD0ddox7D7FSBR7GeHO0gffX6lQ&s",
+          url: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=80",
           alt: "Full Spectrum High Efficiency White LED Grow Light"
         }
       ],
